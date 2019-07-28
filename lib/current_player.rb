d36276f6ae5795453d68d036fe871a810202c200
John Karrell Speck <speckworks@gmail.com>
2019-07-28 18:41:24 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,17 +1,4 @@
 #turn_count
-def turncount(board)
-count = 0
-  board.each do |spot|
-    if spot == " "
-    puts "not a turn"
-    elsif spot == "X"
-    count = count+1
-    elsif spot == "O"
-    count = count+1
-  end
-  end
- puts count
-end
-turncount(board)
+
 
 #current_player
