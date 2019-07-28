694a6666770ef7a1ac74b6c2157a6bdf9d6e22c4
John Karrell Speck <speckworks@gmail.com>
2019-07-28 18:45:50 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,1 +1,15 @@
 #current_player
+def current_player(board)
+count = 0
+  board.each do |spot|
+    if spot == " "
+    puts "Play Your Turn"
+    elsif spot == "X"
+    count = count+1
+    elsif spot == "O"
+    count = count+1
+  end
+  end
+ puts count
+end
+current_player(board)
