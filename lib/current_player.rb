cc40201d0b67c62721e2355fb172e99891310492
John Karrell Speck <speckworks@gmail.com>
2019-07-28 18:39:36 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,17 @@
 #turn_count
-counter = 0
-if playingTicTac
-  counter = counter + 1
+def turncount(board)
+count = 0
+  board.each do |spot|
+    if spot == " "
+    puts "not a turn"
+    elsif spot == "X"
+    count = count+1
+    elsif spot == "O"
+    count = count+1
+  end
+  end
+ puts count
 end
+turncount(board)
 
 #current_player
